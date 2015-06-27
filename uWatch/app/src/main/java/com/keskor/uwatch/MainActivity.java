1b879c5ab4f7a951655392370120bf5b3004ef85
Martha Mohlala <martha.mohlala@gmail.com>
2015-06-27 17:35:22 +0200

    Progress on Front-end

--- uWatch/app/src/main/java/com/keskor/uwatch/MainActivity.java
+++ uWatch/app/src/main/java/com/keskor/uwatch/MainActivity.java
@@ -1,19 +1,37 @@
-package com.example.martina.uwatch;
+package com.example.keskor.uwatch;
 
+
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+    int numberOfLoginAttempts = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login(setContentView(R.layout.activity_main));
+        Button registerButton = (Button) findViewById(R.id.registerbtn);
+        // Listening to register new account link
+        registerButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // Switching to Register screen
+                Intent i = new Intent(getApplicationContext(), com.example.keskor.uwatch.Register.class);
+                startActivity(i);
+            }
+        });
     }
 
     @Override
@@ -41,17 +59,32 @@ public class MainActivity extends ActionBarActivity {
 
     EditText username = (EditText)findViewById(R.id.editText2);
     EditText password = (EditText)findViewById(R.id.editText);
-    void login(View view)
-    {
-        if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin"))
-        {
-            System.out.println("Success");
-        }
-        else
-        {
-            System.out.println("incorrect login");
-            setContentView(R.layout.activity_register);
+    Button login = (Button)findViewById(R.id.button);
+    TextView loginLockedTV = (TextView)findViewById(R.id.loginLockedTV);
+    TextView attemptsLeftTV = (TextView)findViewById(R.id.attemptsLeftTV);
+    TextView numberOfRemainingLoginAttemptsTV = (TextView) findViewById(R.id.numberOfRemainingLoginAttemptsTV);
 
+    public void login(View view)
+    {
+        if (username.getText().toString().equals("admin")&&
+        password.getText().toString().equals("admin"))
+        {
+        Toast.makeText(getApplicationContext(), "Hello admin!",
+                Toast.LENGTH_SHORT).show();
+    } else
+        {
+        Toast.makeText(getApplicationContext(), "Incorrect login!",
+                Toast.LENGTH_SHORT).show();
+        numberOfLoginAttempts--;
+        attemptsLeftTV.setVisibility(View.VISIBLE);
+        numberOfRemainingLoginAttemptsTV.setVisibility(View.VISIBLE);
+        numberOfRemainingLoginAttemptsTV.setText(Integer.toString(numberOfLoginAttempts));
+        if (numberOfLoginAttempts == 0) {
+            login.setEnabled(false);
+            loginLockedTV.setVisibility(View.VISIBLE);
+            loginLockedTV.setBackgroundColor(Color.RED);
+            loginLockedTV.setText("LOGIN LOCKED!!!");
         }
+    }
     }
 }
--- uWatch/app/src/main/java/com/keskor/uwatch/Register.java
+++ uWatch/app/src/main/java/com/keskor/uwatch/Register.java
@@ -1,17 +1,86 @@
-package com.example.martina.uwatch;
+package com.example.keskor.uwatch;
+
+
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
 
 
 public class Register extends ActionBarActivity {
+
+    EditText editTextFirstName;
+    EditText editTextLastName;
+    EditText editTextEmail;
+    EditText editTextUserName;
+    EditText editTextPassword;
+    LoginDataBaseAdapter loginDataBaseAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+
+
+        loginDataBaseAdapter = new LoginDataBaseAdapter(this);
+        loginDataBaseAdapter = loginDataBaseAdapter.open();
+
+
+        editTextFirstName =(EditText)findViewById(R.id.fname);
+        editTextLastName =(EditText)findViewById(R.id.lname);
+        editTextEmail = (EditText)findViewById(R.id.editTextPassword);
+        editTextUserName=(EditText)findViewById(R.id.editTextPassword);
+        editTextPassword=(EditText)findViewById(R.id.editTextConfirmPassword);
+
+        Button btnCreateAccount=(Button)findViewById(R.id.register);
+        btnCreateAccount.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+
+                String fname = editTextFirstName.getText().toString();
+                String lname = editTextLastName.getText().toString();
+                String uname = editTextUserName.getText().toString();
+                String email = editTextEmail.getText().toString();
+                String pass = editTextPassword.getText().toString();
+
+
+                if(fname.equals("") || lname.equals("") || uname.equals("") || email.equals("") || pass.equals("")) {
+                    Toast.makeText(getApplicationContext(), "Fill in the fields", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                else {
+
+                    loginDataBaseAdapter.insertEntry(uname, pass);
+                    Toast.makeText(getApplicationContext(), "Registered Successfully", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        Button loginScreen = (Button) findViewById(R.id.bktologin);
+
+        // Listening to Login Screen link
+        loginScreen.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+                // Closing registration screen
+                // Switching to Login Screen/closing register screen
+                finish();
+            }
+        });
     }
 
     @Override
@@ -35,4 +104,5 @@ public class Register extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
