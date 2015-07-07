389c78bc8a0005fdcfee6856bc1c55e2da14f14b
10416260 <keskor.pro@gmail.com>
2015-07-07 13:46:51 +0200

    Addded Java Files

---
+++ uWatchV_2/uWatch/app/src/main/java/com/keskor/uwatch/Configuration.java
@@ -0,0 +1,10 @@
+package com.keskor.uwatch;
+
+/**
+ * Created by Keskor on 04-Jul-15.
+ */
+public class Configuration
+{
+    public static String ServerURL = "http://196.249.19.2/upload.php";
+   //public static String IMAGE_DIRECTORY_NAME = "/"
+}
