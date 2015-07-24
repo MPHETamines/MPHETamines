51446becfcca5651cc2b4f0881e03cae329d607b
Maphuti <u12310043@tuks.co.za>
2015-07-24 18:20:41 +0200

    Firebase
    Now you can upload and download pde from firebase cloud

--- uWatchV3/www/js/app.js
+++ uWatchV3/www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 
 var myService = angular.module("starter", ["ionic", "ngCordova", "firebase"]);
-var fb = new Firebase("https://glaring-torch-4614.firebaseio.com/");
+var fb = new Firebase("https://torrid-heat-8946.firebaseio.com/");
 //var myService = angular.module('starter', ['ionic','ngCordova'])
 
 myService.run(function($ionicPlatform) {
@@ -25,36 +25,172 @@ myService.run(function($ionicPlatform) {
   });
 });
 
-
 /*
-  This is the main controller for the app, please define all your methods in here 
-*/
 
-myService.controller("MasterController", function($scope, $cordovaCamera, $cordovaMedia) {
-  //capturing a picture method
-    $scope.takePicture = function() {
+imageApp.controller("SecureController", function($scope, $ionicHistory, $firebaseArray, $cordovaCamera) {
+
+    $ionicHistory.clearHistory();
+
+    $scope.images = [];
+
+    var fbAuth = fb.getAuth();
+    if(fbAuth) {
+        var userReference = fb.child("users/" + fbAuth.uid);
+        var syncArray = $firebaseArray(userReference.child("images"));
+        $scope.images = syncArray;
+    } else {
+        $state.go("firebase");
+    }
+
+    $scope.upload = function() {
         var options = {
             quality : 75,
             destinationType : Camera.DestinationType.DATA_URL,
             sourceType : Camera.PictureSourceType.CAMERA,
             allowEdit : true,
             encodingType: Camera.EncodingType.JPEG,
-            targetWidth: 300,
-            targetHeight: 300,
             popoverOptions: CameraPopoverOptions,
+            targetWidth: 500,
+            targetHeight: 500,
             saveToPhotoAlbum: false
         };
- 
         $cordovaCamera.getPicture(options).then(function(imageData) {
-            console.log("succesfully took a picture" );
-            $scope.imgURI = "data:image/jpeg;base64," + imageData;
-        }, function(err) {
-            // An error occured. Show a message to the user
-            console.log("Error while taking a picture");
+            syncArray.$add({image: imageData}).then(function() {
+                alert("Image has been uploaded");
+            });
+        }, function(error) {
+            console.error(error);
         });
     }
 
- //check internet connection method
+});
+*/
+/*
+  This is the main controller for the app, please define all your methods in here 
+*/
+
+myService.controller("MasterController", function($ionicPopup, $scope, $state, $ionicHistory, $firebaseArray, $firebaseAuth, $cordovaCamera, $cordovaMedia) {
+
+  //capturing a picture method
+    $ionicHistory.clearHistory();
+    $scope.images = [];
+    var fbAuth = fb.getAuth();
+    if(fbAuth) {
+        var userReference = fb.child("users/" + fbAuth.uid);
+        var syncArray = $firebaseArray(userReference.child("images"));
+        $scope.images = syncArray;
+    } else {
+        $state.go("tabs.login");
+    }
+
+//===================== login and register ===========
+    var login = function(username, password) {
+        var fbAuth = $firebaseAuth(fb);
+        fbAuth.$authWithPassword({
+            email: username,
+            password: password
+        }).then(function(authData) {
+            $state.go("secure");
+        }).catch(function(error) {
+            console.error("ERROR: " + error);
+        });
+    };
+
+    var register = function(username, password) {
+        var fbAuth = $firebaseAuth(fb);
+        fbAuth.$createUser({email: username, password: password}).then(function(userData) {
+            return fbAuth.$authWithPassword({
+                email: username,
+                password: password
+            });
+        }).then(function(authData) {
+            $state.go("secure");
+        }).catch(function(error) {
+            console.error("ERROR: " + error);
+        });
+    };
+
+//===================================================
+/*
+//  Dialog box to confirm upload
+     var confirmUpload = function() {
+       var confirmPopup = $ionicPopup.confirm({
+         title: 'Would you like to upload PDE?'
+       });
+       confirmPopup.then(function(res) {
+         if(res) {
+           alert('PDE upload success');
+         } else {
+           alert('PDE was not uploaded');
+         }
+       });
+     };
+
+*/  //popup view
+  var loginView = function() {
+      $scope.data = {}
+      // An elaborate, custom popup
+      var myPopup = $ionicPopup.show({
+        template: '<input ng-model="data.username" type="text" placeholder="Username" /><input ng-model="data.password" type="password" placeholder="Password" />',
+        title: 'Please login',
+        scope: $scope,
+        buttons: [
+          {
+            text: '<b>Login</b>',
+            type: 'button-positive',
+            onTap: function(e) {
+              if (!$scope.data.username || !$scope.data.password) {
+                //don't allow the user to close unless he enters login password
+                e.preventDefault();
+              } else {
+                login($scope.data.username,$scope.data.password);
+                alert("logged in");
+               // return $scope.data;
+              }
+            }
+          },
+          { text: 'Cancel' },
+        ]
+      });
+      myPopup.then(function(res) {
+        console.log('Tapped!', res);
+      });/*
+      $timeout(function() {
+         myPopup.close(); //close the popup after 3 seconds for some reason
+      }, 12000);*/
+     };
+
+//================= capture and upload image ==========
+    $scope.uploadPicture = function() {
+        var options = {
+            quality : 75,
+            destinationType : Camera.DestinationType.DATA_URL,
+            sourceType : Camera.PictureSourceType.CAMERA,
+            allowEdit : true,
+            encodingType: Camera.EncodingType.JPEG,
+            popoverOptions: CameraPopoverOptions,
+            targetWidth: 500,
+            targetHeight: 500,
+            saveToPhotoAlbum: false
+        };
+        $cordovaCamera.getPicture(options).then(function(imageData) {
+          //confirmUpload();
+            if(fbAuth){
+              syncArray.$add({image: imageData}).then(function() {
+                  alert("Image has been uploaded");
+              });
+            }else{
+              loginView();
+            //  $state.go('tabs.login');
+            }
+        }, function(error) {
+           alert(error);
+            console.error(error);
+        });
+    };
+
+
+//============= check internet connection method =================
   var checkConnection = function() {
         var networkState = navigator.connection.type;
         var states = {};
@@ -68,12 +204,12 @@ myService.controller("MasterController", function($scope, $cordovaCamera, $cordo
         states[Connection.NONE]     = 'No network connection';
 
         alert('Connection type: ' + states[networkState]);
-    }
+    };
 
-// Getting the geoLocation method
+//=============  Getting the geoLocation method ========================
     $scope.getLocation = function(){
       var onSuccess = function(position) {
-      alert('Latitude: '          + position.coords.latitude          + '\n' +
+      alert('Latitude: '        + position.coords.latitude          + '\n' +
           'Longitude: '         + position.coords.longitude         + '\n' +
           'Altitude: '          + position.coords.altitude          + '\n' +
           'Accuracy: '          + position.coords.accuracy          + '\n' +
@@ -89,53 +225,48 @@ myService.controller("MasterController", function($scope, $cordovaCamera, $cordo
           'message: ' + error.message + '\n');
       }
       navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    };
+
+//=======  Capture Audio using native record ==============
+   $scope.captureAudio = function(){
+      // capture callback
+      var captureSuccess = function(mediaFiles) {
+          var i, path, len;
+          for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+              path = mediaFiles[i].fullPath;
+              // do something interesting with the file
+          }
+      };
+      // capture error callback
+      var captureError = function(error) {
+          navigator.notification.alert('Error code: ' + error.code, null, 'Capture Error');
+      };
+      // start audio capture
+      navigator.device.capture.captureAudio(captureSuccess, captureError, {limit:1});
     }
 
-//Capture Audio using native record 
- $scope.captureAudio = function(){
-    // capture callback
-    var captureSuccess = function(mediaFiles) {
-        var i, path, len;
-        for (i = 0, len = mediaFiles.length; i < len; i += 1) {
-            path = mediaFiles[i].fullPath;
-            // do something interesting with the file
-        }
-    };
 
-    // capture error callback
-    var captureError = function(error) {
-        navigator.notification.alert('Error code: ' + error.code, null, 'Capture Error');
-    };
+//============ capture image and store in device storage ================
+    $scope.captureImage = function(){
+        // capture callback
+      var captureSuccess = function(mediaFiles) {
+          var i, path, len;
+          for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+              path = mediaFiles[i].fullPath;
+              // do something interesting with the file
+          }
+          if(path)
+              alert(path);
+      };
+      // capture error callback
+      var captureError = function(error) {
+          navigator.notification.alert('Error code: ' + error.code, null, 'Capture Error');
+      };
+      // start image capture
+      navigator.device.capture.captureImage(captureSuccess, captureError, {limit:1});
+    }
 
-    // start audio capture
-    navigator.device.capture.captureAudio(captureSuccess, captureError, {limit:1});
-  }
-
-//capture image and store in device storage
-$scope.captureImage = function(){
-    // capture callback
-  var captureSuccess = function(mediaFiles) {
-      var i, path, len;
-      for (i = 0, len = mediaFiles.length; i < len; i += 1) {
-          path = mediaFiles[i].fullPath;
-          // do something interesting with the file
-
-      }
-      if(path)
-          alert(path);
-  };
-
-  // capture error callback
-  var captureError = function(error) {
-      navigator.notification.alert('Error code: ' + error.code, null, 'Capture Error');
-  };
-
-  // start image capture
-  navigator.device.capture.captureImage(captureSuccess, captureError, {limit:2});
-
-}
-
-//Capture Video using native controller// capture callback
+//========== Capture Video using native controller// capture callback ==========
   $scope.captureVideo = function(){
     var captureSuccess = function(mediaFiles) {
         var i, path, len;
@@ -144,117 +275,12 @@ $scope.captureImage = function(){
             // do something interesting with the file
         }
     };
-
     // capture error callback
     var captureError = function(error) {
         navigator.notification.alert('Error code: ' + error.code, null, 'Capture Error');
     };
-
     // start video capture
     navigator.device.capture.captureVideo(captureSuccess, captureError, {limit:1});
   }
 
-/*
-    MediaError.MEDIA_ERR_ABORTED = 1
-    MediaError.MEDIA_ERR_NETWORK = 2
-    MediaError.MEDIA_ERR_DECODE = 3
-    MediaError.MEDIA_ERR_NONE_SUPPORTED = 4
-*/
-
-/*
-  //Recor audio
-    $scope.recordAudio = function () {
-        var src = "audio/myrecording.mp3";
-        var mediaRec = new Media(src,
-            // success callback
-            function() {
-                console.log("recordAudio():Audio Success");
-            },
-
-            // error callback
-            function(err) {
-                console.log("recordAudio():Audio Error: "+ err.code);
-            });
-
-        // Record audio
-        mediaRec.startRecord();
-    }
-
-    //define all your methods below this line
-
-    // Play audio
-    //This function works, you only need to pass a url then it will play an audio
-    $scope.playAudio=function (src) {
-        // Play the audio file at url
-        var my_media = new Media(src,
-            // success callback
-            function () {
-                console.log("playAudio():Audio Success");
-            },
-            // error callback
-            function (err) {
-                console.log("playAudio():Audio Error: " + err);
-            }
-        );
-        // Play audio
-        my_media.play();
-    }
-*/
 });
-
-
-/*
-  These are all the routes for the app. 
-  tabs, capture, login, about, are all views for the app
-  /tabs - is an abstract view at the bottom of the pages
-  /capture - is a view for capturing pde
-  /login - is a view for user login
-  /about - is a view for details about developers
-*/
-myService.config(function($stateProvider, $urlRouterProvider){
-  $stateProvider
-    .state('tabs',{
-        url:'/tab',
-        abstract:true,
-        templateUrl: 'templates/tabs.html'
-
-    })
-    .state('tabs.capture',{
-        url: '/capture',
-        views:{
-            'tabs-capture':{
-                templateUrl:'templates/capture.html',
-                controller:'MasterController'
-            }
-        }
-    })
-    .state('tabs.view',{
-        url:'/view',
-        views:{
-            'tabs-view':{
-                templateUrl:'templates/view.html',
-            }
-        }
-    })
-
-    .state('tabs.login',{
-        url:'/login',
-        views:{
-            'tabs-login':{
-                templateUrl:'templates/login.html',
-            }
-        }
-    })
-    
-    .state('tabs.register',{
-        url:'/register',
-        views:{
-            'tabs-login':{
-                templateUrl:'templates/register.html',
-            }
-        }
-    })
-    //This is a default view that will load when the app loads
-    $urlRouterProvider.otherwise('/tab/capture');
-});
-
